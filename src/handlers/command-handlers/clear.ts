b60c55099f4267b5220c7b32fc29679433611533
Mrewy <25589237+Mrewy@users.noreply.github.com>
2026-07-17 04:43:55 +0200

    feat: added confirmation popup to clear comand

--- src/handlers/command-handlers/clear.ts
+++ src/handlers/command-handlers/clear.ts
@@ -1,8 +1,13 @@
 /** @format */
 
 import {
+  TextInputBuilder,
+  TextInputStyle,
+  ModalBuilder,
+  LabelBuilder,
   MessageFlags,
   type ChatInputCommandInteraction,
+  type ModalSubmitInteraction,
   type PermissionsBitField,
   type TextChannel,
   type GuildMember,
@@ -22,16 +27,14 @@ export function clearHandler(client: Client) {
     interaction: ChatInputCommandInteraction,
     guild: Readonly<Guild>
   ): Promise<void> => {
-    const defer = interaction.deferReply({ flags: MessageFlags.Ephemeral });
-
     try {
       const interactionGuild = interaction.guild;
 
       if (interactionGuild === null) {
-        await defer;
-        await interaction.editReply(
-          'The bot had to have been added as a server bot for this command to work.'
-        );
+        await interaction.reply({
+          content: 'The bot had to have been added as a server bot for this command to work.',
+          flags: MessageFlags.Ephemeral
+        });
         return;
       }
 
@@ -43,8 +46,10 @@ export function clearHandler(client: Client) {
         // && !administrator(memberRolesCache)
         && !globalAdministrator(member_)
       ) {
-        await defer;
-        await interaction.editReply('You do not have permission to use this command.');
+        await interaction.reply({
+          content: 'You do not have permission to use this command.',
+          flags: MessageFlags.Ephemeral
+        });
         return;
       }
 
@@ -53,8 +58,10 @@ export function clearHandler(client: Client) {
         channel = client.channels.cache.get(interaction.channelId) as TextChannel | undefined;
         if (channel === undefined) throw new Error('Channel not found.');
       } catch {
-        await defer;
-        await interaction.editReply('The command can only be used in a text channel.');
+        await interaction.reply({
+          content: 'The command can only be used in a text channel.',
+          flags: MessageFlags.Ephemeral
+        });
         return;
       }
 
@@ -75,30 +82,79 @@ export function clearHandler(client: Client) {
         || !botPermissionsInChannel.has('ManageMessages')
         || !botPermissionsInChannel.has('ReadMessageHistory')
       ) {
-        await defer;
-        await interaction.editReply(
-          'The bot does not have the permissions to either: view channel, send messages, manage messages or read message history.\n(Replying to command is different from sending messages.)'
-        );
+        await interaction.reply({
+          content:
+            'The bot does not have the permissions to either: view channel, send messages, manage messages or read message history.\n(Replying to command is different from sending messages.)',
+          flags: MessageFlags.Ephemeral
+        });
         return;
       }
+
+      const modalCustomId = 'clearMessageModal';
+      const areYouSureAnswerTextInputCustomId = 'areYouSure';
+      const modal = new ModalBuilder()
+        .setCustomId(modalCustomId)
+        .setTitle('Are You Sure?')
+        .addLabelComponents(
+          new LabelBuilder()
+            .setLabel('Clear Messages')
+            .setDescription("Type 'Yes' to confirm.")
+            .setTextInputComponent(
+              new TextInputBuilder()
+                .setCustomId(areYouSureAnswerTextInputCustomId)
+                .setMaxLength(3)
+                .setStyle(TextInputStyle.Short)
+                .setPlaceholder('Yes')
+                .setRequired(true)
+            )
+        );
+      await interaction.showModal(modal);
+
+      const modalSubmitInteraction = await interaction
+        .awaitModalSubmit({
+          filter: (modalSubmitInteraction_: ModalSubmitInteraction): boolean =>
+            modalSubmitInteraction_.customId === modalCustomId,
+          time: 60000
+        })
+        .catch(() => undefined); //timeout catch
+      if (modalSubmitInteraction === undefined) return;
+
+      const areYouSureAnswer = modalSubmitInteraction.fields.getTextInputValue(
+        areYouSureAnswerTextInputCustomId
+      );
+      if (areYouSureAnswer !== 'Yes') {
+        await modalSubmitInteraction.reply({
+          content: 'Clear messages aborted.',
+          flags: MessageFlags.Ephemeral
+        });
+        return;
+      }
+
+      await modalSubmitInteraction.deferReply({ flags: MessageFlags.Ephemeral });
 
       let messages: Readonly<Collection<string, Readonly<Message<true>>>>;
       do {
         messages = await channel.messages.fetch({ limit: 5 });
 
         const messageDeletes: Promise<void>[] = [];
-        for (const [messageId] of messages) messageDeletes.push(channel.messages.delete(messageId));
+        for (const [messageId] of messages)
+          messageDeletes.push(channel.messages.delete(messageId).catch(() => undefined));
 
         await Promise.all(messageDeletes);
       } while (messages.size > 0);
 
-      await defer;
-      await interaction.editReply('Cleared messages.');
+      await modalSubmitInteraction.editReply('Cleared messages.');
+      return;
     } catch (error) {
       logError(error, 'Error at clearHandler');
 
-      await defer;
-      await interaction.editReply('Failed to clear messages.');
+      if (interaction.replied) return; // if the error happens after interaction.showModal()
+
+      await interaction.reply({
+        content: 'Failed to clear messages.',
+        flags: MessageFlags.Ephemeral
+      });
+      return;
     }
   };
 }

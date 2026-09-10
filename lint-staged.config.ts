0208c195061c62a05b049072c2ce13a06ae40e74
Mrewy <25589237+Mrewy@users.noreply.github.com>
2026-09-10 09:28:06 +0200

    chore(deps): npm-check-updates, package.json engines node change, eslint fix

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -2,7 +2,7 @@
 
 import type { Configuration } from 'lint-staged';
 
-const configuration: Readonly<Configuration> = {
+const configuration: Configuration = {
   '!(*.png)': 'cspell --no-summary --no-progress',
   '*': 'cspell --no-summary --no-progress',
   '*.ts': 'eslint --flag unstable_native_nodejs_ts_config --concurrency=auto',
